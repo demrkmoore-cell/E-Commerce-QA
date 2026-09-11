66eac0e260feb3a718418f6c7288a402a1008b75
demrkmoore-cell <demrkmoore@gmail.com>
2026-09-11 13:19:30 -0500

    Expand Playwright product page object for detail validation

--- ui-tests/pages/product_page.py
+++ ui-tests/pages/product_page.py
@@ -4,7 +4,19 @@ from playwright.sync_api import Page
 class ProductPage:
     def __init__(self, page: Page):
         self.page = page
+        self.product_title = "#tbodyid h2"
+        self.product_price = "#tbodyid h3"
+        self.product_description = "#more-information"
         self.add_to_cart_link = 'a:has-text("Add to cart")'
 
     def add_to_cart(self):
         self.page.locator(self.add_to_cart_link).click()
+
+    def get_title(self):
+        return self.page.locator(self.product_title).inner_text()
+
+    def get_price(self):
+        return self.page.locator(self.product_price).inner_text()
+
+    def get_description(self):
+        return self.page.locator(self.product_description).inner_text()
